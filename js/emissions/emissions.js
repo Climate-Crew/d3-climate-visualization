7fdc4ac92b611495d8a2f7eda813765bc916ac56
Max Urbany <maximilian_urbany@g.harvard.edu>
2021-12-06 03:48:00 -0500

    no message

--- js/emissions/emissions.js
+++ js/emissions/emissions.js
@@ -27,7 +27,7 @@ class StackedAreaChart {
     initVis(){
         let vis = this;
 
-        vis.margin = {top: 20, right: 20, bottom: 230, left: 100};
+        vis.margin = {top: 50, right: 0, bottom: 230, left: 200};
 
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom
